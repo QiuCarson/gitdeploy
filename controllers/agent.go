e1ede47e3487d318856749f27300e3727a7700cf
qiuyisong <qiuyisong@gmail.com>
2017-06-26 13:35:19 +0800

    v1

--- controllers/agent.go
+++ controllers/agent.go
@@ -89,3 +89,76 @@ func (this *AgentController) validServer(server *models.Server) error {
 
 	return nil
 }
+
+// 编辑
+func (this *AgentController) Edit() {
+	id, _ := this.GetInt("id")
+	server, err := new(models.Server).GetServer(id, models.SERVER_TYPE_AGENT)
+	this.checkError(err)
+
+	if this.isPost() {
+		server.Ip = this.GetString("server_ip")
+		server.Area = this.GetString("area")
+		server.SshPort, _ = this.GetInt("ssh_port")
+		server.SshUser = this.GetString("ssh_user")
+		server.SshPwd = this.GetString("ssh_pwd")
+		server.SshKey = this.GetString("ssh_key")
+		server.WorkDir = this.GetString("work_dir")
+		server.Description = this.GetString("description")
+		err := this.validServer(server)
+		this.checkError(err)
+		err = new(models.Server).UpdateServer(server)
+		this.checkError(err)
+		//service.ActionService.Add("edit_agent", this.auth.GetUserName(), "server", server.Id, server.Ip)
+		this.redirect(beego.URLFor("AgentController.List"))
+	}
+
+	this.Data["pageTitle"] = "编辑跳板机"
+	this.Data["server"] = server
+	this.display()
+}
+
+// 删除
+func (this *AgentController) Del() {
+	id, _ := this.GetInt("id")
+
+	_, err := new(models.Server).GetServer(id, models.SERVER_TYPE_AGENT)
+	this.checkError(err)
+
+	err = new(models.Server).DeleteServer(id)
+	this.checkError(err)
+	//service.ActionService.Add("del_agent", this.auth.GetUserName(), "server", id, "")
+
+	this.redirect(beego.URLFor("AgentController.List"))
+}
+
+// 项目列表
+func (this *AgentController) Projects() {
+	id, _ := this.GetInt("id")
+	server, err := new(models.Server).GetServer(id, models.SERVER_TYPE_AGENT)
+	this.checkError(err)
+	envList, err := new(models.EnvServer).GetEnvListByServerId(id)
+	this.checkError(err)
+
+	result := make(map[int]map[string]interface{})
+	for _, env := range envList {
+		if _, ok := result[env.ProjectId]; !ok {
+			project, err := new(models.Project).GetProject(env.ProjectId)
+			if err != nil {
+				continue
+			}
+			row := make(map[string]interface{})
+			row["projectId"] = project.Id
+			row["projectName"] = project.Name
+			row["envName"] = env.Name
+			result[env.ProjectId] = row
+		} else {
+			result[env.ProjectId]["envName"] = result[env.ProjectId]["envName"].(string) + ", " + env.Name
+		}
+	}
+
+	this.Data["list"] = result
+	this.Data["server"] = server
+	this.Data["pageTitle"] = server.Ip + " 下的项目列表"
+	this.display()
+}
